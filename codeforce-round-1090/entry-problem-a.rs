534dae1781adeec307a703ee698771eefee687a6
will H <wustracean@gmail.com>
2026-07-07 19:34:21 +0100

    Update entry-problem-a.rs

--- codeforce-round-1090/entry-problem-a.rs
+++ codeforce-round-1090/entry-problem-a.rs
@@ -1,1 +1,17 @@
 
+use std::io::{self, Read};
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+
+    let mut it = input.split_whitespace();
+
+    let t: usize = it.next().unwrap().parse().unwrap();
+    for i in 0..t {
+        let x : i32 = it.next().unwrap().parse().unwrap();
+        println!("67");
+    }
+}
+
+
